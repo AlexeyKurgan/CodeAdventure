30ac6d3de5be663d2c3e8252a0009b3eef7b471f
Alexey Kurgan <alexkurgan19927@gmail.com>
2021-02-16 16:02:35 +0200

    continue working with changing theme

--- src/js/themeColor.js
+++ src/js/themeColor.js
@@ -4,42 +4,39 @@ const themeBtn = document.querySelectorAll('.footer__settings-theme-btn');
 
 function setThemeColor(btnArr){
   btnArr.map((item)=>{
+
     item.addEventListener('click',(e)=>{
-      // const storage = {
-      //   selectedTheme:false
-      // }
-      // localStorage.setItem('theme_color',JSON.stringify(storage));
+      let selectedTheme = {
+        light:header.className + ' theme_light',
+        dark:footer.className,
+      };
 
-      let selectedTheme = false;
-     
-      localStorage.setItem('selectedTheme',selectedTheme);
+      console.log(selectedTheme)
+    // let headerClass = header.classList.contains('theme_light');
+    // localStorage.setItem('header',header.className);
+    // localStorage.setItem('footer',footer.className);
 
-
-      // console.log(localStorage.getItem('theme_color'));
-      
-
-      if(e.target.id === 'lt' && selectedTheme === false){
+    let res = localStorage.getItem('header') + ' "header" ';
+    let res2 = localStorage.getItem('footer') + ' "header" ';
+    console.log(res)
+    console.log(res2)
+      // localStorage.setItem('selectedTheme',JSON.stringify(selectedTheme));
+      // let theme = JSON.parse(localStorage.getItem('selectedTheme'));
         
-        // storage.light = true;
-        // storage.dark = false;
-
-        // console.log(storage);
-        // localStorage.selectedTheme = true;
-        console.log(localStorage.selectedTheme);
-        // console.log(localStorage.getItem('theme_color'));
-        // localStorage.setItem('light_theme','test')
-        // localStorage.setItem('test1', header.classList.add('theme_light'))
-        // const lightTheme = {
-        //   header: header.classList.add('theme_light'),
-        //   footer: footer.classList.add('theme_light'),
-        //   'lightTheme-button': btnArr[0].classList.add('active'),
-        //   'darkTheme-button': btnArr[1].classList.remove('active'),
-        // }
-        
+        // let lightTheme = header.className.split(' ').slice(1).join();
         // console.log(lightTheme);
+      // if(e.target.id === 'lt' &&  theme.light === false){
+      // if(e.target.id === 'lt' && headerClass === false){
+      if(e.target.id === 'lt'){
+       
         
-        // console.log(header.setAttribute('theme','light_theme'));
-        // console.log(header.getAttribute('theme'));
+        // theme.light = true;
+        // theme.dark = false;
+        // localStorage.setItem('selectedTheme',JSON.stringify(theme));
+       
+        
+
+
 
         header.classList.add('theme_light');
         footer.classList.add('theme_light');
@@ -47,24 +44,42 @@ function setThemeColor(btnArr){
         btnArr[1].classList.remove('active'); 
 
         
-        // let storage = {
-        //   light:e.target.outerHTML,
-        //   btn1 : btnArr[0].outerHTML,
-        //   btn2 : btnArr[1].outerHTML,
-        // };
-        
-        // localStorage.setItem('theme_light', JSON.stringify(storage))
-        // console.log(JSON.parse(localStorage.getItem('theme_light')));
-      }
 
-     
-     
+        // console.dir(header.className)
+        // console.log(header);
+        // console.log(footer);
+        console.log(header.classList.contains('theme_light'))
+        console.log(footer.classList.contains('theme_light'))
+
+
+        // localStorage.setItem('header', header.classList.contains('theme_light'))
+      }
+      // console.log(theme.light + ' light')
+      // console.log(theme.dark + ' dark')
+
+      // if(e.target.id === 'dk' && theme.dark){
       if(e.target.id === 'dk'){
+      // if(e.target.id === 'dk' && headerClass){
+        // localStorage.setItem('header', header.classList.contains('theme_light'))
+        // theme.dark === false
+        // theme.light = false;
+        // theme.dark = true;
+        // localStorage.setItem('selectedTheme',JSON.stringify(theme));
+
         header.classList.remove('theme_light');
         footer.classList.remove('theme_light');
         btnArr[1].classList.add('active');
         btnArr[0].classList.remove('active');
+
+        // console.dir(header.className)
+        // console.log(header);
+        // console.log(footer);
+        console.log(header.classList.contains('theme_light'))
+        console.log(footer.classList.contains('theme_light'))
       }
+
+      // let lightTheme = header.className.split(' ').slice(1).join();
+      // console.log(lightTheme);
     })
   })
   
