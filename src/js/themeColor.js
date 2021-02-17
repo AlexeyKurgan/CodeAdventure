cf3f733ecc1eedc79f099100fd11896e57e4b086
Alexey Kurgan <alexkurgan19927@gmail.com>
2021-02-17 16:37:35 +0200

    theme color script finished work and modified sign up btn style

--- src/js/themeColor.js
+++ src/js/themeColor.js
@@ -1,26 +1,43 @@
 const header = document.querySelector('.header');
 const footer = document.querySelector('.footer');
-const themeBtns = document.querySelectorAll('.footer__settings-theme-btn');
- 
-function setThemeColor(btns) {
-  let isLight = localStorage.getItem('isLight');
-      updateTheme();
- 
-  btns.forEach((btn) => {
-    btn.addEventListener('click', (ev) => {
-      isLight = ev.target.id === 'lt';
-      localStorage.setItem('isLight', isLight || '');
-      updateTheme();
-    });
-  });
- 
-  function updateTheme() {
-    [header, footer].forEach(el =>
-      el.classList[isLight ? 'add' : 'remove']('theme_light'));
- 
-    btns[0].classList[isLight ? 'add' : 'remove']('active');
-    btns[1].classList[isLight ? 'remove' : 'add']('active');
-  }
+const btns = document.querySelectorAll('.footer__settings-theme-btn');
+
+function setThemeColor(btns){
+  updateTheme();
+
+  btns.forEach(item => {
+    item.addEventListener('click', e => {
+
+      switch(e.target.id){
+        case 'lt':
+          localStorage.setItem('theme_light', 'theme_light')
+          updateTheme();
+        break;
+
+        case 'dk':
+          localStorage.setItem('theme_light', '')
+          updateTheme();
+        break; 
+      }
+
+    })
+  })
 }
+
+function updateTheme(){
+  let lightTheme = localStorage.getItem('theme_light');
  
-setThemeColor(themeBtns);
+  if(lightTheme){
+    header.classList.add(lightTheme);
+    footer.classList.add(lightTheme);
+    btns[0].classList.add('active');
+    btns[1].classList.remove('active');
+    return;
+  }
+    header.classList.remove('theme_light');
+    footer.classList.remove('theme_light');
+    btns[1].classList.add('active');
+    btns[0].classList.remove('active');
+}
+
+setThemeColor(btns);
