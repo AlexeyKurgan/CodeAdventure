cc0acd69b9b4da7ba5d7180dfe097ce17ae34a4d
Alex <you@example.com>
2021-03-17 22:41:43 +0200

    finished header mobile section

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,6 +1,10 @@
-const mobileMenu = document.querySelector('.header__burger-menu');
+const burgerMenu = document.querySelector('.header__burger-menu');
+const siteNavMobile = document.querySelector('.header__site-nav-mobile');
+const siteNavMobileBottom = document.querySelector('.header__site-nav-mobile-bottom');
+const actionsContainer = document.querySelectorAll('.header__actions-container');
 
-mobileMenu.addEventListener('click', e => {
+burgerMenu.addEventListener('click', e => {
+
   if(e.target.nodeName === 'DIV'){
     const span = [...e.path[0].children];
     span.forEach(item => {
@@ -13,5 +17,8 @@ mobileMenu.addEventListener('click', e => {
     span.forEach(item => {
       item.classList.toggle('burger-active');
       });
-  } 
+  }
+  siteNavMobile.classList.toggle('header__site-nav-mobile_active');
+  siteNavMobileBottom.classList.toggle('header__site-nav-mobile-bottom_active');
+  actionsContainer[1].classList.toggle('header__actions-container_active');
 })
